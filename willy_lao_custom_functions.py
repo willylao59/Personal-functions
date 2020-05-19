fbd0843cf87541b1bc42540c0db5539e9b86e4f3
Willy LAO <lao.willy@gmail.com>
2020-05-19 21:59:38 +0200

    Création de data_display_dask

--- willy_lao_custom_functions.py
+++ willy_lao_custom_functions.py
@@ -51,7 +51,7 @@ def name_var(obj, namespace):
 
 ############################################################################
 
-def data_display(data, data_names):    
+def data_display_pandas(data, data_names):    
     
     import pandas as pd
     import ipywidgets as widgets
@@ -61,6 +61,45 @@ def data_display(data, data_names):
     for j in range(len(data)) :
         data_j_head     = data[j].head()
         data_j_describe = data[j].describe()
+        data_j          = pd.concat([data_j_head, data_j_describe], sort=False)  
+        dict_display.update({data_names[j]: data_j})
+
+    def f(Data):
+        return dict_display[Data]
+    
+    widgets.interact(f, Data=data_names)
+    return f         
+
+
+# In[ ]:
+
+
+############################################################################
+
+# Function call: data_display(data)
+# Purpose      : Returns the name of a variable in a string
+
+# Inputs       : data       = list of DASK dataframes
+#                data_names = list of the names of the DASK dataframes 
+# Outputs      : Display a table with the head() and describe() of the data
+
+# Creator      : Willy LAO
+# Last update  : 17/05/2020
+# Version      : Python 3
+
+############################################################################
+
+def data_display_dask(data, data_names):    
+    
+    from dask import dataframe as daskdf
+    import ipywidgets as widgets
+    from ipywidgets import interactive
+    import pandas as pd
+    
+    dict_display = {}
+    for j in range(len(data)) :
+        data_j_head     = data[j].head()
+        data_j_describe = data[j].describe().compute()
         data_j          = pd.concat([data_j_head, data_j_describe], sort=False)  
         dict_display.update({data_names[j]: data_j})
 
